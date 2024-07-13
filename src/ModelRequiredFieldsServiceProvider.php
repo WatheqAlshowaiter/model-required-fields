ab0c189d1f93297dfadad35e4a6ab8ad68a47270
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2024-07-13 13:38:58 +0300

    Make the package production ready 🚀

---
+++ src/ModelRequiredFieldsServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace WatheqAlshowaiter\ModelRequiredFields;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class ModelRequiredFieldsServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+
+        /*
+         * This class is a Package Service Provider
+         *
+         * More info: https://github.com/spatie/laravel-package-tools
+         */
+
+        $package
+            ->name('model-required-fields');
+    }
+}
