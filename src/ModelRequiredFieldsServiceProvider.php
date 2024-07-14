618b60b4a782ab505106f8cdd7ae901635c042e2
WatheqAlshowaiter <WatheqAlshowaiter@users.noreply.github.com>
2024-07-14 11:28:31 +0000

    Fix styling

--- src/ModelRequiredFieldsServiceProvider.php
+++ src/ModelRequiredFieldsServiceProvider.php
@@ -13,6 +13,6 @@ class ModelRequiredFieldsServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
     }
 }
