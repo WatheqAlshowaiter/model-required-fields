302c6249dd36aab464323648b3baf876dc4c503c
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2024-07-21 04:20:22 +0300

    debug: add more tests for sqlserver results

--- tests/RequiredFieldsTest.php
+++ tests/RequiredFieldsTest.php
@@ -18,13 +18,13 @@ class RequiredFieldsTest extends TestCase
         ], Father::getRequiredFields());
     }
 
-    // public function test_get_required_fields_for_father_model_for_older_versions()
-    // {
-    //     $this->assertEquals([
-    //         'name',
-    //         'email',
-    //     ], Father::getRequiredFieldsForOlderVersions());
-    // }
+    public function test_get_required_fields_for_father_model_for_older_versions()
+    {
+        $this->assertEquals([
+            'name',
+            'email',
+        ], Father::getRequiredFieldsForOlderVersions());
+    }
 
     // public function test_get_required_fields_in_order()
     // {
